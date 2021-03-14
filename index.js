b076a02b7562b0f28cc74f1cc1b9ad99126ce00e
TerryThreatt <terry.threatt@gmail.com>
2021-03-13 18:44:50 -0600

    configured apollo server

---
+++ index.js
@@ -0,0 +1,24 @@
+const { ApolloServer } = require('apollo-server')
+const gql = require('graphql-tag')
+
+const typeDefs = gql`
+    type Query{
+        sayHi: String!
+    }
+`
+
+const resolvers = {
+    Query: {
+        sayHi = () => 'Hello World!'
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+})
+
+server.listen({ port: 5000})
+    .then(res => {
+        console.log(`Server running at ${res.url}`)
+    })
