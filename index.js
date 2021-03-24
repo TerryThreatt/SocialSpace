0b010d713e27ba0319cae0414bdc421c4847aaa2
TerryThreatt <terry.threatt@gmail.com>
2021-03-24 18:29:07 -0500

    split grapphql code up

--- index.js
+++ index.js
@@ -2,20 +2,11 @@ const { ApolloServer } = require('apollo-server')
 const gql = require('graphql-tag')
 const mongoose = require('mongoose')
 
+const typeDefs = require('./graphql/typeDefs')
 const Post = require('./models/Post')
 const { DB } = require('./config')
 
-const typeDefs = gql`
-    type Post {
-        id: ID!
-        body: String!
-        createdAt: String!
-        username: String!
-    }
-    type Query {
-        getPosts: [Post]
-    }
-`
+
 
 const resolvers = {
     Query: {
