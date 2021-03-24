f94e05ae6d0834e2396258344401627c95b9b533
TerryThreatt <terry.threatt@gmail.com>
2021-03-24 18:35:41 -0500

    update resolvers

--- index.js
+++ index.js
@@ -3,23 +3,10 @@ const gql = require('graphql-tag')
 const mongoose = require('mongoose')
 
 const typeDefs = require('./graphql/typeDefs')
-const Post = require('./models/Post')
+const resolvers = require('./graphql/resolvers')
 const { DB } = require('./config')
 
 
-
-const resolvers = {
-    Query: {
-        async getPosts(){
-            try {
-                const posts = await Post.find()
-                return posts
-            } catch(err) {
-                throw new Error(err)
-            }
-        }
-    }
-}
 
 const server = new ApolloServer({
     typeDefs,
