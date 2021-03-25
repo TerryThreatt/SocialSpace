757ac52394367c296d88a8343b720d575edc79a4
TerryThreatt <terry.threatt@gmail.com>
2021-03-24 19:04:49 -0500

    added upser mutation

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -1,10 +1,38 @@
-const User = require("../../models/User")
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
 
+
+const { SECRET_KEY } = require("../../config")
+const User = require("../../models/User")
 
 module.exports = {
     Mutation: {
-        register(_, args, context, info){
-            // TODO: Validate user data 
+        async register(_, { registerInput: { username, email, password, confirmPassword}}, context, info){
+            // TODO: Validate user data
+            // TODO: Make sure user doesnt already exist
+            // TODO: hash password and create auth token
+            password = await bcrypt.hash(password, 12)
+
+            const newUser = new User({
+                email,
+                username,
+                password,
+                createdAt: new Date().toISOString
+            })
+
+            const res = await newUser.save()
+
+            const token = jwt.sign({
+                id: res.id,
+                email: res.email,
+                username: res.username
+            }, SECRET_KEY, { expiresIn: '1h'})
+
+            return {
+                ...res._doc,
+                id: res._id,
+                token
+            }
         }
     }
 }
