4876a8e4e2ed2e7fdfc83d8459d1f2e846c534b1
TerryThreatt <terry.threatt@gmail.com>
2021-04-01 13:52:11 -0500

    updated register user mutation

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -1,5 +1,6 @@
 const bcrypt = require("bcrypt")
 const jwt = require("jsonwebtoken")
+const { UserInputError } = require('apollo-server')
 
 
 const { SECRET_KEY } = require("../../config")
@@ -7,9 +8,18 @@ const User = require("../../models/User")
 
 module.exports = {
     Mutation: {
-        async register(_, { registerInput: { username, email, password, confirmPassword}}, context, info){
+        async register(_, { registerInput: { username, email, password, confirmPassword}}){
             // TODO: Validate user data
             // TODO: Make sure user doesnt already exist
+            const user = await User.findOne({ username })
+            if(user) {
+                throw new UserInputError("Username is taken", {
+                    errors: {
+                        username: 'This username is taken'
+                    }
+                })
+            }
+
             password = await bcrypt.hash(password, 12)
 
             const newUser = new User({
