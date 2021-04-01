cc89fb1f703d56a44e428936b1542fec589ff89a
TerryThreatt <terry.threatt@gmail.com>
2021-04-01 14:34:36 -0500

    added validation

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -2,15 +2,30 @@ const bcrypt = require("bcrypt")
 const jwt = require("jsonwebtoken")
 const { UserInputError } = require('apollo-server')
 
-
+const validateRegisterInput = require('../../utils/validation')
 const { SECRET_KEY } = require("../../config")
 const User = require("../../models/User")
 
 module.exports = {
     Mutation: {
-        async register(_, { registerInput: { username, email, password, confirmPassword}}){
-            // TODO: Validate user data
-            // TODO: Make sure user doesnt already exist
+        async register(
+            _,
+            {
+              registerInput: { username, email, password, confirmPassword }
+            }
+          ) {
+            // Validate user data
+            const { valid, errors } = validateRegisterInput(
+              username,
+              email,
+              password,
+              confirmPassword
+            );
+            if (!valid) {
+              throw new UserInputError('Errors', { errors });
+            }
+
+            // check for multiple users
             const user = await User.findOne({ username })
             if(user) {
                 throw new UserInputError("Username is taken", {
