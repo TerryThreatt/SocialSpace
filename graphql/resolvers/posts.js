802dafbee46129cf00d4d7c9327c96cb1f03db6f
TerryThreatt <terry.threatt@gmail.com>
2021-04-15 18:26:56 -0500

    added deletePost() mutation

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,11 +1,12 @@
 const Post = require('../../models/Post')
 const checkAuth = require('../../utils/check-auth')
+const { AuthenticationError } = require('apollo-server')
 
 module.exports = {
     Query: {
         async getPosts(){
             try {
-                const posts = await Post.find().sort({ createdAt: -1})
+                const posts = await Post.find().sort({ createdAt: -1 })
                 return posts
             } catch(err) {
                 throw new Error(err)
@@ -39,6 +40,23 @@ module.exports = {
             const post = await newPost.save()
 
             return post
+        },
+
+        async deletePost(_, { postId }, context) {
+            const user = checkAuth(context)
+            try {
+                const post = await Post.findById(postId)
+
+                if(user.username === post.username) {
+                    await post.delete()
+
+                    return 'Post deleted sucessfully'
+                } else {
+                    throw new AuthenticationError('Action not allowed')
+                }
+            } catch(err) {
+                throw new Error(err)
+            }
         }
     }
 }
--- utils/check-auth.js
+++ utils/check-auth.js
@@ -1,12 +1,12 @@
-const { AuthenticationError } = require('apollo-server');
+const { AuthenticationError } = require('apollo-server')
 
-const jwt = require('jsonwebtoken');
-const { SECRET_KEY } = require('../config');
+const jwt = require('jsonwebtoken')
+const { SECRET_KEY } = require('../config')
 
 module.exports = (context) => {
   // context = { ... headers }
   const authHeader = context.req.headers.authorization
-  
+
   if (authHeader) {
     // Bearer ....
     const token = authHeader.split('Bearer ')[1]
