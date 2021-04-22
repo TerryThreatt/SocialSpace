1ede079720a3c32e1d0f648ef68e53115bc95667
TerryThreatt <terry.threatt@gmail.com>
2021-04-22 15:08:46 -0500

    created deleteComment() resolver

--- graphql/resolvers/comments.js
+++ graphql/resolvers/comments.js
@@ -1,4 +1,4 @@
-const {  UserInputError } = require('apollo-server')
+const {  UserInputError, AuthenticationError } = require('apollo-server')
 
 const Post = require("../../models/Post")
 const User = require('../../models/User')
@@ -8,7 +8,7 @@ const checkAuth = require("../../utils/check-auth")
 module.exports = {
     Mutation: {
         createComment: async (_, { postId, body }, context ) => {
-            const { username }= checkAuth(context)
+            const { username } = checkAuth(context)
 
             if(body.trim() === '') {
                 throw new UserInputError('Empty comment', {
@@ -30,6 +30,26 @@ module.exports = {
                 await post.save()
 
                 return post
+            } else {
+                throw new UserInputError("Post not found")
+            }
+        },
+
+        async deleteComment(_, { postId, commentId}, context) {
+            const { username } = checkAuth(context)
+
+            const post = await Post.findById(postId)
+
+            if(post) {
+                const commentIndex = post.comments.findIndex(c => c.id === commentId)
+
+                if(post.comments[commentIndex].username === username){
+                    post.comments.splice(commentIndex, 1)
+                    await post.save()
+                    return post
+                } else {
+                    throw new AuthenticationError("Action not allowed")
+                }
             } else {
                 throw new UserInputError("Post not found")
             }
