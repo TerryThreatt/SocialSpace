c6f1fcadd1b6906154a4f314e0f5baade814ad3f
TerryThreatt <terry.threatt@gmail.com>
2021-04-22 15:18:40 -0500

    added likePost() mutation

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,6 +1,6 @@
 const Post = require('../../models/Post')
 const checkAuth = require('../../utils/check-auth')
-const { AuthenticationError } = require('apollo-server')
+const { AuthenticationError, UserInputError } = require('apollo-server')
 
 module.exports = {
     Query: {
@@ -56,6 +56,29 @@ module.exports = {
                 }
             } catch(err) {
                 throw new Error(err)
+            }
+        },
+        async likePost(_, { postId }, context) {
+            const { username } = checkAuth(context)
+
+            const post = await Post.findById(postId)
+
+            if(post) {
+                if(post.likes.find(like => like.username === username)){
+                    // Post already liked, unlike it
+                    post.likes = post.likes.filter(like => like.username !== username)
+                } else {
+                    // Not liked, like post
+                    post.likes.push({
+                        username,
+                        createdAt: new Date().toISOString()
+                    })
+                }
+
+                await post.save()
+                return post
+            } else {
+                throw new UserInputError("Post not found")
             }
         }
     }
