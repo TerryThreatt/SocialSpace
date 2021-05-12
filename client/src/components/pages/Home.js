b2c2fc8a06bf4e7522b553375f7edd09bcb363dd
TerryThreatt <terry.threatt@gmail.com>
2021-05-12 18:05:52 -0500

    add FETCH_POSTS_QUERY

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,9 +1,33 @@
 import React from 'react'
+import { useQuery } from '@apollo/client'
+import gql from 'graphql-tag'
 
-export default function Home() {
+function Home() {
+    const { loading, data } = useQuery(FETCH_POSTS_QUERY)
+
+    if(data) {
+        console.log(data)
+    }
     return (
         <div>
             <h1>Home</h1>
         </div>
     )
 }
+
+const FETCH_POSTS_QUERY = gql`
+    {
+        getPosts{
+        id body createdAt username likeCount
+        likes{
+            username
+        }
+        commentCount
+        comments{
+            id username createdAt body
+        }
+        }
+    }
+`
+
+export default Home
