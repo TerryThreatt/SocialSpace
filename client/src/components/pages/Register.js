c683c46f7c210f514599cc43e10bbd63f661f51f
TerryThreatt <terry.threatt@gmail.com>
2021-05-30 12:00:06 -0500

    stub out register form

--- client/src/components/pages/Register.js
+++ client/src/components/pages/Register.js
@@ -1,9 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Form } from 'semantic-ui-react'
 
-export default function Register() {
+function Register() {
+    const [values, setValues] = useState({
+        username: "",
+        email: "",
+        password: "",
+        confirmPassword: ""
+    })
+
     return (
         <div>
-            <h1>Register</h1>
+            <Form onSubmit={onSubmit} noValidate>
+                <h1>Register</h1>
+                <Form.Input
+                    label="Username"
+                    placeholder="Username"
+                    name="username"
+                    value={values.username}
+                    onChange={onChange}
+                />
+                <Form.Input
+                    label="Email"
+                    placeholder="Email"
+                    name="email"
+                    value={values.email}
+                    onChange={onChange}
+                />
+                <Form.Input
+                    label="Password"
+                    placeholder="Password"
+                    name="password"
+                    value={values.password}
+                    onChange={onChange}
+                />
+                <Form.Input
+                    label="Confirm Password"
+                    placeholder="Confirm Password"
+                    name="confirmPassword"
+                    value={values.confirmPassword}
+                    onChange={onChange}
+                />
+            </Form>
         </div>
     )
 }
+
+export default Register
